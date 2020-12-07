78d30ddb0eaab5aa2e80b2615d83153752ec3521
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-07 08:30:35 -0300

    Access the database and show in screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-    <header className="main-header">Eu amo muito uma mulher mas ela não compartilha dos meus sentimentos, não vou obrigá-la a me amar mas vou cercá-la com meu amor enquanto rexo pela sua felicidade!</header>
+    <header className="main-header">Eu amo muito uma mulher mas ela não compartilha dos meus sentimentos, não vou obrigá-la a me amar mas vou cercá-la com meu amor enquanto rezo pela sua felicidade!</header>
 );
 export default Header;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,6 +4,10 @@ import Api from './../../services/Api'
 
 export default class Main extends Component {
 
+    state = {
+        products:[]
+    }
+
     componentDidMount(){
         this.loadProducts();
     }//method executed as soon as the component enters in screen
@@ -11,9 +15,16 @@ export default class Main extends Component {
     loadProducts = async()=>{
         const response = await Api.get('/products');
         console.log(response.data.docs);
+        this.setState({products:response.data.docs})
     };
 
     render(){
-        return <h1>Hello World!</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(prod =>{
+                    return (<h2 key={prod._id}>{prod.title}</h2>);
+                })}
+            </div>
+        )
     }
 }
